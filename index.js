bbdf334217fae7b22c799c697dd7c64bcee13495
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-08-10 21:28:07 +0300

    FIX: Confgured file uploads using multer

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/static', express.static('static'));
 app.use(router)
 
 // mongodb setup 
