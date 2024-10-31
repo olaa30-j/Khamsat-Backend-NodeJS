b2fc479f0d44e6097c5d8beb97fa497ab5d1732b
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-10-31 10:04:19 +0300

    FEAT: paypal integration

--- router/payment.js
+++ router/payment.js
@@ -1,6 +1,8 @@
 import express from "express"
-import { createPaymentIntent } from "../controller/payment.js"
+import { createPaymentIntent, handleCaptureOrder, handleCreateOrder } from "../controller/payment.js"
 const router = express.Router()
 
 router.post("/create-payment-intent", createPaymentIntent)
+router.post("/create-paypal-order", handleCreateOrder)
+router.post("/order/:orderID/capture", handleCaptureOrder)
 export default router
