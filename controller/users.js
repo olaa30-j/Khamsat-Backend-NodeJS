0c2b869d2421a43f6faf5c0324e888cd229f2231
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-11-04 22:32:03 +0200

    FIX: set auth token

--- controller/users.js
+++ controller/users.js
@@ -23,7 +23,8 @@ export const login = async (req, res) => {
     res.cookie("authToken", token, {
       maxAge: 24 * 60 * 60 * 1000, 
       httpOnly: true,               
-      secure: process.env.NODE_ENV === 'production', 
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       path: '/',                    
   });
 
@@ -71,6 +72,7 @@ export const create = async (req, res) => {
       maxAge: 24 * 60 * 60 * 1000, 
       httpOnly: true,               
       secure: process.env.NODE_ENV === 'production', 
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       path: '/',                    
   });
     res.status(200).json({ message: "Success", data: { user, token } });
